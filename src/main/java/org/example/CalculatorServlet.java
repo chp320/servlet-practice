c9a1cb67eba2af197b0b40df60c2762aa8bb4789
chp320 <chp320@naver.com>
2024-04-15 21:45:30 +0900

    servlet interface 구현 -> 필요하지 않지만 Servlet 에서 필수로 구현하게 하기 때문에 불필요한 부분이 존재함

--- src/main/java/org/example/CalculatorServlet.java
+++ src/main/java/org/example/CalculatorServlet.java
@@ -1,34 +1,47 @@
 package org.example;
 
+import org.example.calculate.Calculator;
+import org.example.calculate.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/calculate")
 public class CalculatorServlet implements Servlet {
     private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
+    private ServletConfig servletConfig;    // 인스턴스 변수 초기화
 
     @Override
-    public void init(ServletConfig config) throws ServletException {
+    public void init(ServletConfig servletConfig) throws ServletException {
         log.info("init");
+        this.servletConfig = servletConfig;
     }
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         log.info("service");
+        int operand1 = Integer.parseInt(req.getParameter("operand1"));
+        String operator = req.getParameter("operator");
+        int operand2 = Integer.parseInt(req.getParameter("operand2"));
+
+        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        PrintWriter writer = res.getWriter();
+        writer.println(result);
     }
 
     @Override
     public void destroy() {
-
+        // 자원 해제하는 기능 수행
     }
 
     @Override
     public ServletConfig getServletConfig() {
-        return null;
+        return this.servletConfig;
     }
 
     @Override
